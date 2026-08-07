21adefee41365d0d62ddefca4abdff4c5353c5eb
linenfeng323-pixel <linenfeng323@gmail.com>
2026-08-07 19:06:27 +0800

    fix

---
+++ xposed-stub/de/robv/android/xposed/callbacks/XC_LoadPackage.java
@@ -0,0 +1,10 @@
+package de.robv.android.xposed.callbacks;
+
+/**
+ * 存根 - 转发到 de.robv.android.xposed.XC_LoadPackage
+ */
+public class XC_LoadPackage extends de.robv.android.xposed.XC_LoadPackage {
+    public XC_LoadPackage() {
+        super();
+    }
+}
